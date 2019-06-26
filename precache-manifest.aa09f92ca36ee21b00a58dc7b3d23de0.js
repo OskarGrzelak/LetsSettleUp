09d79735cbad46d2295b2ffbd24712cba145cedb
OskarGrzelak <oskargrzelak@gmail.com>
2019-06-26 12:13:12 +0200

    Updates

---
+++ precache-manifest.aa09f92ca36ee21b00a58dc7b3d23de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9faaaf91e93eb2510b",
+    "url": "/LetsSettleUp/static/js/runtime~main.226bb855.js"
+  },
+  {
+    "revision": "0858755a79ee33967d20",
+    "url": "/LetsSettleUp/static/js/main.2c1fff4c.chunk.js"
+  },
+  {
+    "revision": "998bfb70c90f56622c9d",
+    "url": "/LetsSettleUp/static/js/2.cae90a66.chunk.js"
+  },
+  {
+    "revision": "0858755a79ee33967d20",
+    "url": "/LetsSettleUp/static/css/main.0aac6861.chunk.css"
+  },
+  {
+    "revision": "1f24d6c8cb02411ca8a9038b5c86ff2c",
+    "url": "/LetsSettleUp/index.html"
+  }
+];
